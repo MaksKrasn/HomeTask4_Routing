7f6cf10990b32320e9085b64b446d88003673f4d
Maks Krasnikov <uw5ekr@gmail.com>
2020-08-26 16:03:39 +0300

    implement components & routing

--- HT4Routing/src/app/app-routing.module.ts
+++ HT4Routing/src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component'
+import { UsersComponent } from './components/users/users.component'
+import { TodosComponent } from './components/todos/todos.component'
+import { AlbumsComponent } from './components/albums/albums.component'
+import { CommentsComponent } from './components/comments/comments.component'
+import { PhotosComponent } from './components/photos/photos.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "posts", component: PostsComponent
+  },
+  {
+    path: "users", component: UsersComponent
+  },
+  {
+    path: "todos", component: TodosComponent
+  },
+  {
+    path: "albums", component: AlbumsComponent
+  },
+  {
+    path: "comments", component: CommentsComponent
+  },
+  {
+    path: "photos", component: PhotosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ HT4Routing/src/app/components/albums/albums.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'albums',
+  templateUrl: './albums.component.html'
+})
+export class AlbumsComponent {
+  title = 'Albums';
+}
