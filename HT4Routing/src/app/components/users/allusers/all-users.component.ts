3c970f719cd539199b0ecd6a0adc7794bc33cb3b
MaksKrasn <uw5ekr@gmail.com>
2020-09-10 21:28:49 +0300

    implement all-users component

--- HT4Routing/src/app/components/users/allusers/all-users.component.ts
+++ HT4Routing/src/app/components/users/allusers/all-users.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JsonPlaceholderService } from 'src/app/services/jsonplaceholder.service';
+import { User } from 'src/app/interfaces/user.interface';
 
 @Component({
   selector: 'all-users',
   templateUrl: './all-users.component.html'
 })
-export class AllUsersComponent {
+export class AllUsersComponent implements OnInit{
   title = 'All-Users';
+  users: User[];
+  headElements = ['ID', 'Name', 'Username', 'Email', 'Address', 'Phone', 'Website', 'Company'];
+
+  constructor(public jsonplaceholderService: JsonPlaceholderService){}
+
+  ngOnInit(): void {
+    this.jsonplaceholderService.getUsers().subscribe(data => {
+      this.users = data;
+      //console.log(this.users[5].company);
+    });
+  }
+
+
 }
--- HT4Routing/src/app/services/jsonplaceholder.service.ts
+++ HT4Routing/src/app/services/jsonplaceholder.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { environment } from 'src/environments/environment'
 import { Post } from '../interfaces/post.interface'
+import { User } from '../interfaces/user.interface'
 
 @Injectable()
 export class JsonPlaceholderService {
@@ -10,5 +11,9 @@ export class JsonPlaceholderService {
 
     getPosts() : Observable<Post[]>{
         return this.httpClient.get<Post[]>(`${environment.apiUrl}/posts`);
+    }
+
+    getUsers() : Observable<User[]>{
+        return this.httpClient.get<User[]>(`${environment.apiUrl}/users`);
     }
 }
