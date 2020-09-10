b51a91686b0ed25afbcf03fc00986a2880353060
Maks Krasnikov <uw5ekr@gmail.com>
2020-09-10 17:30:09 +0300

    222

--- HT4Routing/src/app/components/users/userinfo/user-info.component.ts
+++ HT4Routing/src/app/components/users/userinfo/user-info.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { JsonPlaceholderService } from 'src/app/services/jsonplaceholder.service';
 
 @Component({
   selector: 'user-info',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class UserInfoComponent {
   title = 'User-info';
+  //item: Item;
+    constructor(private jsonPlaceholderService: JsonPlaceholderService,
+        private activatedRoute: ActivatedRoute) {
+        activatedRoute.params.subscribe(prms => {
+            const id = prms['userId'];
+            //this.item = itemsService.getItemById(id);
+            console.log(id);
+        });
+    }
 }
